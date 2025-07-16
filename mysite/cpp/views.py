badbc26491faabc0f6231eafaa88e381fb3bc4e2
Fellipe Aleixo <fellipe.aleixo@ifrn.edu.br>
2025-07-16 17:29:12 -0300

    Implementação do fluxo principal do CDU 004.

--- mysite/cpp/views.py
+++ mysite/cpp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .services import ServicoResposta, ServicoCorrecao
+from .services import ServicoResposta, ServicoCorrecao, ServicoTurma
+from .models import Avaliacao
 
+## Avalia Resposta View
+###############################
 class AvaliaRespostaView(View):
     def get(self, request, *args, **kwargs):
         srv = ServicoResposta()
@@ -19,4 +22,32 @@ class AvaliaRespostaView(View):
         comentario = request.POST.get('comentario', '')
         id_aval = srv.salvar_correcao(id_resposta, id_aluno, nota, comentario)
         return redirect('/avaliacao/{}/'.format(id_aval))
+
+## Nova Avaliação View
+##############################
+class NovaAvaliacaoView(View):
+    def get(self, request, *args, **kwargs):
+        srv = ServicoTurma()
+        id_turma = kwargs.get('id_turma', '')
+        turma = srv.get_turma(id_turma)
+        contexto = {'turma': turma, "tipos": Avaliacao.TIPO_CORRECAO}
+        # TODO: só será exibido o formulário se o professor logado estiver associado à turma
+        return render(request, 'cpp/nova_avaliacao.html', contexto)
+
+    def post(self, request, *args, **kwargs):
+        srv = ServicoTurma()
+        id_turma = kwargs.get('id_turma', '')
+        turma = srv.get_turma(id_turma)
+        # TODO: confirmar que o professor logado está associado a turma em questão
+        if turma:
+            titulo = request.POST.get('titulo', '')
+            enunciado = request.POST.get('enunciado', '')
+            max_nota = request.POST.get('nota', '')
+            tipo_correcao = request.POST.get('correcao', '')
+            if titulo and enunciado and max_nota and tipo_correcao:
+                id_avaliacao = srv.adiciona_avaliacao(
+                    turma, titulo, enunciado, max_nota, tipo_correcao
+                )
+                if id_avaliacao:
+                    return redirect('/avaliacao/{}/'.format(id_avaliacao))
 
